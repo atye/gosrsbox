d810de909acbc403210541b54da3d917710d1b52
Aaron Tye <aarontye@Aarons-MacBook-Pro.local>
2020-12-20 20:19:06 -0500

    update docs

--- osrsboxapi/api/doc.go
+++ osrsboxapi/api/doc.go
@@ -1,7 +1,4 @@
 /*
-Query Items, Monsters, and Prayers from the RESTful API and any JSON file from the Static JSON API.
-```
-api := api.NewAPI(nil)
-```
+Query Items, Monsters, and Prayers from the RESTful API and any JSON file from the Static JSON API
 */
 package api
