325e3130393a6e74dd5ecd19293d1090c9d1c86f
Aaron Tye <aarontye@Aarons-MacBook-Pro.local>
2020-12-20 20:12:40 -0500

    update docs

--- osrsboxapi/api/doc.go
+++ osrsboxapi/api/doc.go
@@ -1,8 +1,7 @@
 /*
 api provides a wrapper for osrsbox-api and osrsbox-db.
 
-Query Items, Monsters, and Prayers from the RESTful API
-Query any JSON file in the STATIC JSON API
+Query Items, Monsters, and Prayers from the RESTful API and any JSON file from the STATIC JSON API.
 
 api := api.NewAPI(nil)
 */
