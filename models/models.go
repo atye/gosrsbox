8c22950f539981d3e7a2c590fb18355e2d134d53
Aaron Tye <aaron.tye@emc.com>
2021-05-26 17:22:17 -0400

    add relevant models

--- models/models.go
+++ models/models.go
@@ -1,6 +1,8 @@
 package models
 
-import "github.com/atye/gosrsbox/internal/api"
+import (
+	"github.com/atye/gosrsbox/internal/api"
+)
 
 type Item = api.Item
 
@@ -15,3 +17,39 @@ type Monster = api.Monster
 type MonsterDrops = api.MonsterDrops
 
 type Prayer = api.Prayer
+
+var (
+	NewItem                      = api.NewItem
+	NewNullableItem              = api.NewNullableItem
+	NewItemEquipment             = api.NewItemEquipment
+	NewNullableItemEquipment     = api.NewNullableItemEquipment
+	NewItemWeapon                = api.NewItemWeapon
+	NewNullableItemWeapon        = api.NewNullableItemWeapon
+	NewItemWeaponStances         = api.NewItemWeaponStances
+	NewNullableItemWeaponStances = api.NewNullableItemWeaponStances
+
+	NewMonster              = api.NewMonster
+	NewNullableMonster      = api.NewNullableMonster
+	NewMonsterDrops         = api.NewMonsterDrops
+	NewNullableMonsterDrops = api.NewNullableMonsterDrops
+
+	NewPrayer        = api.NewPrayer
+	NewNullablePraye = api.NewNullablePrayer
+
+	PtrBool            = api.PtrBool
+	PtrInt             = api.PtrInt
+	PtrInt32           = api.PtrInt32
+	PtrInt64           = api.PtrInt64
+	PtrFloat32         = api.PtrFloat32
+	PtrFloat64         = api.PtrFloat64
+	PtrString          = api.PtrString
+	PtrTime            = api.PtrTime
+	NewNullableBool    = api.NewNullableBool
+	NewNullableInt     = api.NewNullableInt
+	NewNullableInt32   = api.NewNullableInt32
+	NewNullableInt64   = api.NewNullableInt64
+	NewNullableFloat32 = api.NewNullableFloat32
+	NewNullableFloat64 = api.NewNullableFloat64
+	NewNullableString  = api.NewNullableString
+	NewNullableTime    = api.NewNullableTime
+)
